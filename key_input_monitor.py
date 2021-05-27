39e8c880a3ac0eeb73d9db9c7bf0a698a482b0dc
william <willbe921@gmail.com>
2021-05-27 15:13:43 +0800

    key in test

--- key_input_monitor.py
+++ key_input_monitor.py
@@ -3,13 +3,19 @@ import time
 import pyautogui
 
 
+time.sleep(2)
 
 
+# pyautogui.typewrite('Hello world!')
+# pyautogui.typewrite('Hello world!', interval=0.25)
+pyautogui.hotkey('1',interval=1) 
+pyautogui.hotkey('3',interval=1)
+pyautogui.hotkey('5',interval=1)
+pyautogui.hotkey('2',interval=1)
+pyautogui.hotkey('4',interval=1)
 
-pyautogui.typewrite('Hello world!')
-pyautogui.typewrite('Hello world!', interval=0.25)
-pyautogui.hotkey('shift','ctrl', 'left')
-pyautogui.hotkey('ctrl', 'c') 
-pyautogui.press('right')
-pyautogui.press('enter')
-pyautogui.hotkey('ctrl', 'v') 
+# pyautogui.hotkey('shift','ctrl', 'left')
+# pyautogui.hotkey('ctrl', 'c') 
+# pyautogui.press('right')
+# pyautogui.press('enter')
+# pyautogui.hotkey('ctrl', 'v') 
